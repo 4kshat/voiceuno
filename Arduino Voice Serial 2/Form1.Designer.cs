55f1dc057710f64448fc8a105b0a3d22cc4af803
4kshat <asharma6995@gmail.com>
2016-03-09 02:24:14 +0530

    First commit

---
+++ Arduino Voice Serial 2/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Arduino_Voice_Serial_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(375, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Enable Voice Control";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(375, 174);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Disable Voice Control";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(357, 258);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "LOGS:";
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(35, 288);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(167, 53);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Lights ON";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(282, 284);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(160, 57);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Lights OFF";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 353);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Voice Control AI Demo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        public System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Arduino Voice Serial 2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+
+namespace Arduino_Voice_Serial_2
+{
+    public partial class Form1 : Form
+    { 
+        SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+            button2.Enabled = true;
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "Hello", "abaay baaatee baand kar", "Good", "who am i", "i am good", "lights on, sir", "lights off, sir", "Very well sir !", "print my name", "lights on", "lights off", "abaay baaatee on kar" });
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammar(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+
+        }
+
+        private void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+
+            switch (e.Result.Text)
+            {
+                case "Hello":
+                    synthesizer.SpeakAsync("Hello Aakshat. How are you ?");
+                    break;
+                case "Good":
+                    synthesizer.SpeakAsync("Very well sir! ");
+                    MessageBox.Show(":)");
+                    break;
+               case "who am i" :
+                    synthesizer.SpeakAsync("You are my mentor and designer Mister Aakshat, you are a student and you are a cool person");
+                    break;
+                case "i am good, how are you ?":
+                    synthesizer.SpeakAsync("Very well sir! ");
+                    break;
+                case "print my name":
+                    synthesizer.SpeakAsync("Akshaat");
+                    richTextBox1.Text += "\n Akshat";
+                    break;
+                case "abaay baaatee baand kar":
+                    if (true)
+                    {
+                        synthesizer.SpeakAsync("lights off, sir");
+                        serialPort1.Write("k");
+                        richTextBox1.Text += "\n Light Switched OFF";
+                        break;
+                    }
+                case "abaay baaatee on kar":
+                    if (true)
+                    {
+                        synthesizer.SpeakAsync("lights on, sir");
+                        serialPort1.Write("A");
+                        richTextBox1.Text += "\n Light Switched ON";
+                        break;
+                    }
+                    
+               case "lights on":
+                    if (true)
+                    {
+                        synthesizer.SpeakAsync("lights on, sir");
+                        serialPort1.Write("A");
+                        richTextBox1.Text += "\n Light Switched ON";
+                        break;
+                    }
+                case "lights off":
+                    if (true)
+                    {
+                        synthesizer.SpeakAsync("lights off, sir");
+                        serialPort1.Write("k");
+                        richTextBox1.Text += "\n Light Switched OFF";
+                        break;
+                    }
+                    
+
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            recEngine.RecognizeAsyncStop();
+            button2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("A");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("k");
+        }
+    }
+}
